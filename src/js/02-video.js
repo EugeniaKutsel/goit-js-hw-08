402c62cb921b5cd67500d731fd83013c9dedda22
Eugenia Kutsel <jenna.kutsel@gmail.com>
2022-09-27 22:24:26 +0300

    Update

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
+import throttle from 'lodash.throttle';
+import Player from '@vimeo/player'
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+let currentTime = 0;
+
+player.on('timeupdate', throttle(saveTime, 1000));
+
+function saveTime(data) {
+    localStorage.setItem('videoplayer-current-time', data.seconds);
+}
+
+currentTime = localStorage.getItem('videoplayer-current-time');
+player.setCurrentTime(currentTime);
